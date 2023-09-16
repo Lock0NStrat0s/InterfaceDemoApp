207fbc9463480df7815267a084e353effc886fe3
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-16 15:45:51 -0600

    Add project files.

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+public interface IComputerController
+{
+    void Connect();
+    void CurrentKeyPressed();
+}
+
+public class Keyboard
+{
+    public void Connect()
+    {
+
+    }
+
+    public void CurrentKeyPressed()
+    {
+
+    }
+}
+
+public class GameController
+{
+    public void Connect()
+    {
+
+    }
+
+    public void CurrentKeyPressed()
+    {
+
+    }
+}
